d37c6d218608e151dfecc1ef03175b31ca903ba2
Samuel Ballan <sgb4622@gmail.com>
2017-03-15 15:49:28 -0400

    cleaned up exports

--- src/app/quiz/index.ts
+++ src/app/quiz/index.ts
@@ -1,5 +1,7 @@
 export { QuizModule } from './quiz.module';
 export { QuizPanelComponent } from './quiz-panel/quiz-panel.component'
+export { QuizQuestionComponent } from './quiz-question/quiz-question.component'
+export { QuizAnswersComponent } from './quiz-answers/quiz-answers.component'
 
 export * from './models';
 export * from './services';
